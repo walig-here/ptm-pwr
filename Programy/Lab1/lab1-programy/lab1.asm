e9d99b1d02271aa8610e7352d2c0ae2757d2f7de
walig-here <daw.walig@gmail.com>
2023-03-06 19:20:39 +0100

    Zadanie 1 - Lab1

--- Programy/Lab1/lab1-programy/lab1.asm
+++ Programy/Lab1/lab1-programy/lab1.asm
@@ -10,14 +10,14 @@ TASK_CODE	SEGMENT CODE					; kod realizujacy polecenia z listy zadan
 ;---------------------------------------------------------------------
 ; Zadanie 1 - Stale
 ;---------------------------------------------------------------------
-NUM 		EQU			255				; Liczba 2-batjowa (0,511)
-NUM_ADR		EQU			30h				; 
+NUM 		EQU			16				; Liczba 2-batjowa
+NUM_ADR		EQU			30h				; Adres mlodszego bitu liczby NUM
 
 ;=====================================================================
 ;						KOD STARTOWY
 
 			CSEG AT		0
-			LJMP		test_once
+			LJMP		test_once		; test_once lub test_multi
 
 ;=====================================================================
 ; 						TESTY POPRAWNOSCI
@@ -50,25 +50,22 @@ loop:		MOV			DPTR, #8000h		; liczba w komorkach XRAM 8000h i 8001h
 ; ZADANIE 1
 ; Inkrementuje liczbe dwubajtowa zapisana w pamieci IRAM.
 ; Wejscie:	R0 - adres mlodszego bajtu liczby
-; Wyjscie: 	Zinkrementowana liczba zapisana na 2 bajtach pamieci ram
+; Wyjscie: 	Zinkrementowana liczba zapisana na 2 bajtach pamieci IRAM
 ;			od komórki o adresie NUM_ADR
-; Uzywane:	R0, R1, A
+; Uzywane:	R0
 ;---------------------------------------------------------------------
-task1:		
-			MOV			A, R0				; Zapisanie adresu starszego bajtu w rejestrze 1
-			INC			A
-			MOV			R1, A
-			
+task1:	
 			MOV			@R0, #LOW(NUM)		; Wprowadzenie wartosci 2-bajtowej do pamieci
-			MOV			@R1, #HIGH(NUM)
+			INC			R0
+			MOV			@R0, #HIGH(NUM)
+			DEC			R0
 			
 			INC			@R0					; Wlasciwa inkrementacja - wykorzystanie CJNE
 			CJNE		@R0, #0, end_taks1
-			INC			@R1
+			INC			R0
+			INC			@R0
 end_taks1:  
-			CLR			A					; Koniec procdury
-			MOV			R0, #0
-			MOV			R1, #0
+			MOV			R0, #0				; Koniec procdury
 			RET								
 		
 		
