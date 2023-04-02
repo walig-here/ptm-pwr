3ce9589d1acc2ac91511f7f813047a5b35f3ac75
walig-here <daw.walig@gmail.com>
2023-04-02 02:38:23 +0200

    Dodanie programu na lab3

--- Programy/Lab/lab2/lab2.asm
+++ Programy/Lab/lab2/lab2.asm
@@ -4,57 +4,53 @@
 PROCEDURES	SEGMENT	CODE
 UTIL_PROC	SEGMENT CODE
 STACK		SEGMENT	DATA
-	
-			RSEG STACK
-stack_low:	DS				8
 
 ;------------------------------------------------------------------
 ;						BLOK STALYCH
 
-BLOCK_LEN	EQU				5							; dlugosc bufora w pamieci XRAM/IRAM
-XBLOCK_ADR	EQU				0h							; adres poczatku bufora w pamieci XRAM
-IBLOCK_ADR	EQU				30h							; adres poczatku bufora w pamieci IRAM
+BLOCK_LEN	EQU	5								; dlugosc bufora w pamieci XRAM/IRAM
+XBLOCK_ADR	EQU	0h								; adres poczatku bufora w pamieci XRAM
+IBLOCK_ADR	EQU	30h								; adres poczatku bufora w pamieci IRAM
 
 ;------------------------------------------------------------------
 ; 						BLOK STARTOWY
 			
-			CSEG AT			0							
-			MOV				SP, #stack_low
-			SJMP			loop	
+			CSEG AT	0			
+			SJMP loop	
 
-			MOV				DPTR, #XBLOCK_ADR			; Zadanie 1
-			MOV				R2, #BLOCK_LEN
-			LCALL			sum_xram
-			SJMP			loop
+			MOV	DPTR, #XBLOCK_ADR				; Zadanie 1
+			MOV	R2, #BLOCK_LEN
+			LCALL sum_xram
+			SJMP loop
 	
-			MOV				DPTR, #XBLOCK_ADR			; Zadanie 2
-			MOV				R2, #BLOCK_LEN
-			MOV				R0, #IBLOCK_ADR
-			LCALL			copy_xram_iram_inv
-			SJMP			loop
+			MOV	DPTR, #XBLOCK_ADR				; Zadanie 2
+			MOV	R2, #BLOCK_LEN
+			MOV	R0, #IBLOCK_ADR
+			LCALL copy_xram_iram_inv
+			SJMP loop
 				
-			MOV				DPTR, #XBLOCK_ADR			; Zadanie 3
-			MOV				R2, #BLOCK_LEN
-			MOV				R0, #IBLOCK_ADR
-			LCALL			copy_iram_xram_z
-			SJMP			loop	
+			MOV	DPTR, #XBLOCK_ADR				; Zadanie 3
+			MOV	R2, #BLOCK_LEN
+			MOV	R0, #IBLOCK_ADR
+			LCALL copy_iram_xram_z
+			SJMP loop	
 loop:	
-			MOV				DPTR, #XBLOCK_ADR			; Zadanie 4
-			MOV				R2, #BLOCK_LEN
-			MOV				R0, #LOW(000Ah)
-			MOV				R1, #HIGH(000Ah)
-			LCALL			copy_xram_xram_2
-			SJMP			loop
+			MOV	DPTR, #XBLOCK_ADR				; Zadanie 4
+			MOV	R2, #BLOCK_LEN
+			MOV	R0, #LOW(000Ah)
+			MOV	R1, #HIGH(000Ah)
+			LCALL copy_xram_xram_2
+			SJMP loop
 			
-			MOV				R2, #BLOCK_LEN				; Zadanie 5
-			MOV				R0, #IBLOCK_ADR
-			LCALL			count_range
-			SJMP			loop
+			MOV	R2, #BLOCK_LEN					; Zadanie 5
+			MOV	R0, #IBLOCK_ADR
+			LCALL count_range
+			SJMP loop
 
 ;------------------------------------------------------------------
 ; 						BLOK PROCEDUR
 			
-			RSEG			PROCEDURES
+			RSEG PROCEDURES
 
 ;---------------------------------------------------------------------
 ; Sumowanie bloku danych w pamieci zewnetrznej (XRAM)
@@ -64,26 +60,26 @@ loop:
 ; Wyjscie: R7|R6 - 16-bit suma elementow bloku (Hi|Lo)
 ;---------------------------------------------------------------------
 sum_xram:
-			MOV	A, R2						; sprawdzenie czy wskazany blok nie jest zerowej dlugosci
+			MOV	A, R2							; sprawdzenie czy wskazany blok nie jest zerowej dlugosci
 			JZ	reached_sum_buffers_end
 			
-			MOV	R6, #0						; czyszczenie rejestrów wynikowych
+			MOV	R6, #0							; czyszczenie rejestrów wynikowych
 			MOV	R7, #0
 			
-add_next:	CLR	C							; dodanie do mlodszego bajtu zawartosci z bloku danych
+add_next:	CLR	C								; dodanie do mlodszego bajtu zawartosci z bloku danych
 			MOVX A, @DPTR
 			ADD	A, R6				
 			MOV	R6, A
 			
-			MOV	A, R7						; dodanie ewentualnego przeniesienia do starszego bajtu
+			MOV	A, R7							; dodanie ewentualnego przeniesienia do starszego bajtu
 			ADDC A, #0
 			MOV	R7, A
 			
-			INC	DPTR						; przejscie do kolejnej komórki pamieci (o ile nie dotarlismy do konca bufora)
+			INC	DPTR							; przejscie do kolejnej komórki pamieci (o ile nie dotarlismy do konca bufora)
 			DJNZ R2, add_next
 			
 reached_sum_buffers_end:
-			RET								; dotarto do konca bufora
+			RET									; dotarto do konca bufora
 			
 		
 ;---------------------------------------------------------------------
@@ -95,20 +91,20 @@ reached_sum_buffers_end:
 ;          R2   - dlugosc kopiowanego obszaru
 ;---------------------------------------------------------------------
 copy_xram_iram_inv:		
-			MOV				A, R2						; sprzawdzenie, czy wskazany blok nie jest zerowej dlugosci
-			JZ				reached_cpy_xram_buffers_end
+			MOV	A, R2							; sprzawdzenie, czy wskazany blok nie jest zerowej dlugosci
+			JZ reached_cpy_xram_buffers_end
 			
-			ADD				A, R0						; przesuniecie wskaznika IRAM na koniec obszaru docelowego
-			DEC				A
-			MOV				R0, A
+			ADD	A, R0							; przesuniecie wskaznika IRAM na koniec obszaru docelowego
+			DEC	A
+			MOV	R0, A
 			
 copy_next_xram:
-			MOVX			A, @DPTR					; kopiowanie danych z XRAM do IRAM
-			MOV				@R0, A
+			MOVX A, @DPTR						; kopiowanie danych z XRAM do IRAM
+			MOV	@R0, A
 			
-			DEC				R0							; przesuniecie wskaznikow 
-			INC				DPTR
-			DJNZ			R2, copy_next_xram
+			DEC	R0								; przesuniecie wskaznikow 
+			INC	DPTR
+			DJNZ R2, copy_next_xram
 
 reached_cpy_xram_buffers_end:
 			RET
@@ -123,20 +119,20 @@ reached_cpy_xram_buffers_end:
 ;          R2   - dlugosc kopiowanego obszaru
 ;---------------------------------------------------------------------
 copy_iram_xram_z:
-			MOV				A, R2						; sprzawdzenie, czy wskazany blok nie jest zerowej dlugosci
-			JZ				reached_cpy_iram_buffers_end
+			MOV	A, R2							; sprzawdzenie, czy wskazany blok nie jest zerowej dlugosci
+			JZ reached_cpy_iram_buffers_end
 
 copy_next_iram:
-			MOV				A, @R0						; sprawdzenie, czy wartosc komorki jest niezerowa
-			JZ				iram_value_is_zero
+			MOV	A, @R0							; sprawdzenie, czy wartosc komorki jest niezerowa
+			JZ iram_value_is_zero
 			
-														; wykonanie kopii do XRAM i przesuniecie wskaznika w XRAM
-			MOVX			@DPTR, A
-			INC				DPTR
+												; wykonanie kopii do XRAM i przesuniecie wskaznika w XRAM
+			MOVX @DPTR, A
+			INC DPTR
 			
 iram_value_is_zero:
-			INC				R0							; przesuniecie wskaznika w IRAM, przejscie do kopiowania kolejnej komorki
-			DJNZ			R2, copy_next_iram
+			INC R0								; przesuniecie wskaznika w IRAM, przejscie do kopiowania kolejnej komorki
+			DJNZ R2, copy_next_iram
 
 reached_cpy_iram_buffers_end:
 			RET
@@ -150,23 +146,23 @@ reached_cpy_iram_buffers_end:
 ;          R2    - dlugosc kopiowanego obszaru
 ;---------------------------------------------------------------------
 copy_xram_xram_2:
-			MOV				A, R2						; sprawdzenie, czy wskazany blok nie jest zerowej dlugosci
-			JZ				reached_xram_end_2
+			MOV	A, R2							; sprawdzenie, czy wskazany blok nie jest zerowej dlugosci
+			JZ reached_xram_end_2
 			
 			MOV P2, R1
 copy_next_xram_to_xram:
-			MOVX			A, @DPTR					; kopiuje dane do miejca docelowego
-			MOVX			@R0, A
+			MOVX A, @DPTR						; kopiuje dane do miejca docelowego
+			MOVX @R0, A
 			
-			LCALL			increment_r1r0_pointer		; zwiekszam adres miejsca docelowego, zachowujac jednoczesnie adres miejsca zrodlowego
+			LCALL increment_r1r0_pointer		; zwiekszam adres miejsca docelowego, zachowujac jednoczesnie adres miejsca zrodlowego
 			
-			JZ				check_cpy_next				; jezeli element jest niezerowy do kopiuje go drugi raz
-			MOVX			@R0, A
-			LCALL			increment_r1r0_pointer
+			JZ check_cpy_next					; jezeli element jest niezerowy do kopiuje go drugi raz
+			MOVX @R0, A
+			LCALL increment_r1r0_pointer
 			
 check_cpy_next:
-			INC				DPTR
-			DJNZ			R2, copy_next_xram_to_xram
+			INC DPTR
+			DJNZ R2, copy_next_xram_to_xram
 reached_xram_end_2:
 			RET
 
@@ -179,24 +175,24 @@ reached_xram_end_2:
 ; Wyjscie: A  - liczba elementow spelniajacych warunek
 ;---------------------------------------------------------------------
 count_range:
-			MOV				A, R2							; sprawdzenie, czy wskazany blok nie jest zerowej dlugosci
-			JZ				reached_count_end
-			CLR				A							; wyzerowanie licznika
+			MOV	A, R2							; sprawdzenie, czy wskazany blok nie jest zerowej dlugosci
+			JZ reached_count_end
+			CLR	A								; wyzerowanie licznika
 			
 analyze_next:
 			
-			CJNE			@R0, #10, compared_to_10				; sprawdzenie, czy @R0<10
+			CJNE @R0, #10, compared_to_10		; sprawdzenie, czy @R0<10
 compared_to_10:
-			JC				check_loop_condition			
+			JC check_loop_condition			
 										
-			CJNE			@R0, #101, compared_to_100 				; sprawdzenie, czy @R0>100
+			CJNE @R0, #101, compared_to_100 	; sprawdzenie, czy @R0>100
 compared_to_100:
-			JNC				check_loop_condition
+			JNC check_loop_condition
 			
-			INC				A										; zliczenie komórki
+			INC	A								; zliczenie komórki
 check_loop_condition:
-			INC				R0
-			DJNZ			R2, analyze_next
+			INC	R0
+			DJNZ R2, analyze_next
 			
 reached_count_end:
 			RET
---
+++ Programy/Lab/lab3/lab3.asm
@@ -0,0 +1,179 @@
+;---------------------------------------------------------------------------
+;		# SEGMENTY #
+
+PROCEDURES	SEGMENT CODE
+
+;---------------------------------------------------------------------------
+;		# BLOK STALYCH #
+
+LEDS	EQU	P1								; diody LED na P1 (0=ON)
+	
+TIME_MS	EQU	50								; czas w [ms]
+CYCLES	EQU	(1000 * TIME_MS)				; czas w cyklach (f = 12 MHz)
+LOAD	EQU	(65536 - CYCLES - 15)			; wartosc ladowana do TH0|TL0
+
+RIGHT	EQU	0								; kierunek przesuwania LED
+
+;---------------------------------------------------------------------------
+;		# BLOK ZMIENNYCH #
+
+DSEG AT 30h
+counter_50:	DS 1							; licznik w ramach sekundy
+second: DS 1								; sekundy
+minute: DS 1								; minuty
+hour: DS 1 									; godziny
+
+;---------------------------------------------------------------------------
+;		# BLOK STARTOWY #
+
+		CSEG AT 0
+		SJMP loop
+		
+
+		LCALL delay_50ms					
+		SJMP loop
+		
+		MOV R7, #0FFh
+		LCALL delay_nx50ms					
+		SJMP loop
+		
+		LCALL delay_timer_50ms
+		SJMP loop
+loop:		
+		LCALL init_time
+continue_update_time:
+		LCALL delay_timer_50ms	
+		LCALL update_time
+		SJMP continue_update_time
+
+;---------------------------------------------------------------------------
+;		# BLOK PROCEUDUR #
+
+		RSEG PROCEDURES
+
+;---------------------------------------------------------------------------
+; Opoznienie 50 ms (zegar 12 MHz)
+;---------------------------------------------------------------------------
+delay_50ms:									; 2 cykle
+		MOV R0, #97							; 1 cykl
+			
+outer_loop:
+		MOV R1, #255						; 1 cykl * 97
+inner_loop:
+		DJNZ R1, inner_loop					; 2 cykle * 255 * 97
+		DJNZ R0, outer_loop					; 2 cykle * 97
+	
+		MOV R0, #116						; 1 cykl
+complementary_loop:
+		DJNZ R0, complementary_loop			; 2 cykle * 119
+	
+		NOP
+		RET									; 2 cykle
+		
+		
+;---------------------------------------------------------------------
+; Opoznienie n * 50 ms (zegar 12 MHz)
+; R7 - czas x 50 ms
+;---------------------------------------------------------------------
+delay_nx50ms:		
+		DEC R7								; 1 cykl | zmniejszam o 1, gdyz w wypadku wykonania pelnych n odliczen zawsze bedziemy mieli blad 6us, to za duzo!
+		MOV A, R7							; 1 cykl
+		JZ start_counting_last_50ms			; 2 cykle, jezeli mamy wykonac zero 0 plenych wywolan delay_50ms, to od razu przechodze do odliczania dopelniajacego
+
+next_50ms:
+		LCALL delay_50ms					; 50k cykli * R7
+		DJNZ R7, next_50ms					; 2 cykle * R7
+
+start_counting_last_50ms:
+		MOV R0, #97							; 1 cykl | trzeba dopelnic odliczanie ostatnich 50ms
+outer_loop_last_50ms:
+		MOV R1, #255						; 1 cykl * 97
+inner_loop_last_50ms:
+		DJNZ R1, inner_loop_last_50ms		; 2 cykle * 255 * 97
+		DJNZ R0, outer_loop_last_50ms		; 2 cykle * 97
+		
+		MOV R0, #115						; 1 cykl
+final_loop_nx50ms:
+		DJNZ R0, final_loop_nx50ms			; 2 cykle * 115
+		
+		NOP									; 1 cykl
+		RET
+		
+		
+;---------------------------------------------------------------------
+; Opoznienie 50 ms z uzyciem Timera 0 (zegar 12 MHz)
+;---------------------------------------------------------------------
+delay_timer_50ms:							; 2 cykle
+		
+		CLR TR0								; zatrzymuje licznik | 1 cykl
+		
+		MOV A, #00001001b					; wlaczam licznik 0 w trybie 16-bitowym | 1 cykl
+		ORL TMOD, A							; 1 cykl
+		
+		MOV TL0, #LOW(LOAD)					; wczytuje wartosci do rejestrow licznika | 2 cykle
+		MOV TH0, #HIGH(LOAD)				; 2 cykle
+		
+		CLR TF0								; wyczyszczenie flagi przepelnienia licznika | 1 cykl
+		
+		SETB TR0							; wlaczenie timera | 1 cykl
+		
+wait_for_overflow:
+		JNB TF0, wait_for_overflow			; czekam na odliczenie czasu przez timer
+
+		RET									; 2 cykle
+		
+		
+;---------------------------------------------------------------------
+; Inicjowanie czasu w zmiennych: HOUR, MIN, SEC, SEC_100
+;---------------------------------------------------------------------
+init_time:
+		
+		MOV	counter_50, #20
+		MOV second, #59
+		MOV minute, #59
+		MOV hour, #23
+		
+		RET
+		
+
+;---------------------------------------------------------------------
+; Aktualizacja czasu w postaci (HOUR : MIN : SEC) | CNT_50
+; Przy wywolywaniu procedury co 50 ms
+; wykonywana jest aktualizacja czasu rzeczywistego
+;
+; Wyjscie: CY - sygnalizacja zmiany sekund (0 - nie, 1 - tak)
+;---------------------------------------------------------------------
+update_time:
+	CLR F0
+	
+	DEC counter_50							; aktualizacja licznika 50ms, jezeli nie osiagnelismy 0, to nie aktualizujemy dalej czasu
+	MOV A, counter_50
+	JNZ stop_time_update
+	MOV	counter_50, #20
+	SETB F0
+	
+	INC	second								; aktualizacja licznika sekund, jezeli nie osiagnelismy 60 sekund, to nie aktualizujemy dalej czasu
+	MOV A, second
+	XRL A, #60
+	JNZ stop_time_update
+	MOV second, A
+	
+	INC	minute								; aktualizacja licznika minut, jezeli nie osiagnelismy 60 minut, to nie aktualizujemy dalej czasu
+	MOV A, minute
+	XRL A, #60
+	JNZ stop_time_update
+	MOV minute, A
+	
+	INC	hour								; aktualizacja licznika godzin, jezeli nie osiagnelismy 24 godzin, to nie aktualizujemy dalej czasu
+	MOV A, hour
+	XRL A, #24
+	JNZ stop_time_update
+	MOV hour, A
+	
+	
+stop_time_update:	
+	MOV C, F0
+	RET
+		
+;---------------------------------------------------------------------------
+		END
