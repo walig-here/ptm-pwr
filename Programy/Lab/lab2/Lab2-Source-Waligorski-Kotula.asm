e77a160d2337c7303340f2d3544c31656a374f76
walig-here <daw.walig@gmail.com>
2023-04-18 02:08:14 +0200

    Dodanie notatek i programow z lab4

---
+++ Programy/Lab/lab2/Lab2-Source-Waligorski-Kotula.asm
@@ -0,0 +1,217 @@
+;------------------------------------------------------------------
+;						BLOK SEGMENTÓW
+
+PROCEDURES	SEGMENT	CODE
+UTIL_PROC	SEGMENT CODE
+STACK		SEGMENT	DATA
+
+;------------------------------------------------------------------
+;						BLOK STALYCH
+
+BLOCK_LEN	EQU	5								; dlugosc bufora w pamieci XRAM/IRAM
+XBLOCK_ADR	EQU	0h								; adres poczatku bufora w pamieci XRAM
+IBLOCK_ADR	EQU	30h								; adres poczatku bufora w pamieci IRAM
+
+;------------------------------------------------------------------
+; 						BLOK STARTOWY
+			
+			CSEG AT	0			
+			SJMP loop	
+
+			MOV	DPTR, #XBLOCK_ADR				; Zadanie 1
+			MOV	R2, #BLOCK_LEN
+			LCALL sum_xram
+			SJMP loop
+	
+			MOV	DPTR, #XBLOCK_ADR				; Zadanie 2
+			MOV	R2, #BLOCK_LEN
+			MOV	R0, #IBLOCK_ADR
+			LCALL copy_xram_iram_inv
+			SJMP loop
+				
+			MOV	DPTR, #XBLOCK_ADR				; Zadanie 3
+			MOV	R2, #BLOCK_LEN
+			MOV	R0, #IBLOCK_ADR
+			LCALL copy_iram_xram_z
+			SJMP loop	
+loop:	
+			MOV	DPTR, #XBLOCK_ADR				; Zadanie 4
+			MOV	R2, #BLOCK_LEN
+			MOV	R0, #LOW(000Ah)
+			MOV	R1, #HIGH(000Ah)
+			LCALL copy_xram_xram_2
+			SJMP loop
+			
+			MOV	R2, #BLOCK_LEN					; Zadanie 5
+			MOV	R0, #IBLOCK_ADR
+			LCALL count_range
+			SJMP loop
+
+;------------------------------------------------------------------
+; 						BLOK PROCEDUR
+			
+			RSEG PROCEDURES
+
+;---------------------------------------------------------------------
+; Sumowanie bloku danych w pamieci zewnetrznej (XRAM)
+;
+; Wejscie: DPTR  - adres poczatkowy bloku danych
+;          R2    - dlugosc bloku danych
+; Wyjscie: R7|R6 - 16-bit suma elementow bloku (Hi|Lo)
+;---------------------------------------------------------------------
+sum_xram:
+			MOV	A, R2							; sprawdzenie czy wskazany blok nie jest zerowej dlugosci
+			JZ	reached_sum_buffers_end
+			
+			MOV	R6, #0							; czyszczenie rejestrów wynikowych
+			MOV	R7, #0
+			
+add_next:	CLR	C								; dodanie do mlodszego bajtu zawartosci z bloku danych
+			MOVX A, @DPTR
+			ADD	A, R6				
+			MOV	R6, A
+			
+			MOV	A, R7							; dodanie ewentualnego przeniesienia do starszego bajtu
+			ADDC A, #0
+			MOV	R7, A
+			
+			INC	DPTR							; przejscie do kolejnej komórki pamieci (o ile nie dotarlismy do konca bufora)
+			DJNZ R2, add_next
+			
+reached_sum_buffers_end:
+			RET									; dotarto do konca bufora
+			
+		
+;---------------------------------------------------------------------
+; Kopiowanie bloku z pamieci zewnetrznej (XRAM) do wewnetrznej (IRAM)
+; Przy kopiowaniu powinna byc odwrocona kolejnosc elementow
+;
+; Wejscie: DPTR - adres poczatkowy obszaru zrodlowego
+;          R0   - adres poczatkowy obszaru docelowego
+;          R2   - dlugosc kopiowanego obszaru
+;---------------------------------------------------------------------
+copy_xram_iram_inv:		
+			MOV	A, R2							; sprzawdzenie, czy wskazany blok nie jest zerowej dlugosci
+			JZ reached_cpy_xram_buffers_end
+			
+			ADD	A, R0							; przesuniecie wskaznika IRAM na koniec obszaru docelowego
+			DEC	A
+			MOV	R0, A
+			
+copy_next_xram:
+			MOVX A, @DPTR						; kopiowanie danych z XRAM do IRAM
+			MOV	@R0, A
+			
+			DEC	R0								; przesuniecie wskaznikow 
+			INC	DPTR
+			DJNZ R2, copy_next_xram
+
+reached_cpy_xram_buffers_end:
+			RET
+
+
+;---------------------------------------------------------------------
+; Kopiowanie bloku z pamieci wewnetrznej (IRAM) do zewnetrznej (XRAM)
+; Przy kopiowaniu powinny byc pominiete elementy zerowe
+;
+; Wejscie: R0   - adres poczatkowy obszaru zrodlowego
+;          DPTR - adres poczatkowy obszaru docelowego
+;          R2   - dlugosc kopiowanego obszaru
+;---------------------------------------------------------------------
+copy_iram_xram_z:
+			MOV	A, R2							; sprzawdzenie, czy wskazany blok nie jest zerowej dlugosci
+			JZ reached_cpy_iram_buffers_end
+
+copy_next_iram:
+			MOV	A, @R0							; sprawdzenie, czy wartosc komorki jest niezerowa
+			JZ iram_value_is_zero
+			
+												; wykonanie kopii do XRAM i przesuniecie wskaznika w XRAM
+			MOVX @DPTR, A
+			INC DPTR
+			
+iram_value_is_zero:
+			INC R0								; przesuniecie wskaznika w IRAM, przejscie do kopiowania kolejnej komorki
+			DJNZ R2, copy_next_iram
+
+reached_cpy_iram_buffers_end:
+			RET
+			
+;---------------------------------------------------------------------
+; Kopiowanie bloku danych w pamieci zewnetrznej (XRAM -> XRAM)
+; Przy kopiowaniu elementy niezerowe powinny byc podwojone
+;
+; Wejscie: DPTR  - adres poczatkowy obszaru zrodlowego
+;          R1|R0 - adres poczatkowy obszaru docelowego
+;          R2    - dlugosc kopiowanego obszaru
+;---------------------------------------------------------------------
+copy_xram_xram_2:
+			MOV	A, R2							; sprawdzenie, czy wskazany blok nie jest zerowej dlugosci
+			JZ reached_xram_end_2
+			
+			MOV P2, R1
+copy_next_xram_to_xram:
+			MOVX A, @DPTR						; kopiuje dane do miejca docelowego
+			MOVX @R0, A
+			
+			LCALL increment_r1r0_pointer		; zwiekszam adres miejsca docelowego, zachowujac jednoczesnie adres miejsca zrodlowego
+			
+			JZ check_cpy_next					; jezeli element jest niezerowy do kopiuje go drugi raz
+			MOVX @R0, A
+			LCALL increment_r1r0_pointer
+			
+check_cpy_next:
+			INC DPTR
+			DJNZ R2, copy_next_xram_to_xram
+reached_xram_end_2:
+			RET
+
+;---------------------------------------------------------------------
+; Zliczanie w bloku danych w pamieci wewnetrznej (IRAM)
+; liczb mieszczacych sie w przedziale domknietym <10,100>
+;
+; Wejscie: R0 - adres poczatkowy bloku danych
+;          R2 - dlugosc bloku danych
+; Wyjscie: A  - liczba elementow spelniajacych warunek
+;---------------------------------------------------------------------
+count_range:
+			MOV	A, R2							; sprawdzenie, czy wskazany blok nie jest zerowej dlugosci
+			JZ reached_count_end
+			CLR	A								; wyzerowanie licznika
+			
+analyze_next:
+			
+			CJNE @R0, #10, compared_to_10		; sprawdzenie, czy @R0<10
+compared_to_10:
+			JC check_loop_condition			
+										
+			CJNE @R0, #101, compared_to_100 	; sprawdzenie, czy @R0>100
+compared_to_100:
+			JNC check_loop_condition
+			
+			INC	A								; zliczenie komórki
+check_loop_condition:
+			INC	R0
+			DJNZ R2, analyze_next
+			
+reached_count_end:
+			RET
+
+;------------------------------------------------------------------
+; 						BLOK PROCEDUR POMOCNICZYCH
+			
+			RSEG			UTIL_PROC
+
+;------------------------------------------------------------------
+; Inkrementuje 16-bitowy wskaznik R1|R0
+;------------------------------------------------------------------
+increment_r1r0_pointer:
+			INC R0
+			CJNE R0, #0, end_increment
+			INC P2
+
+end_increment:
+			RET
+		
+;------------------------------------------------------------------
+			END
---
+++ Programy/Lab/lab4/lab4.a51
@@ -0,0 +1,166 @@
+WR_CMD		EQU	0FF2Ch		; zapis rejestru komend
+WR_DATA		EQU	0FF2Dh		; zapis rejestru danych
+RD_STAT		EQU	0FF2Eh		; odczyt rejestru statusu
+RD_DATA		EQU	0FF2Fh		; odczyt rejestru danych
+
+ORG 0
+
+	lcall	lcd_init		; inicjowanie wyswietlacza
+
+	mov	A, #04h				; x = 4, y = 0
+	lcall	lcd_gotoxy		; przejscie do pozycji (4, 0)
+
+	mov	DPTR, #text_hello	; wyswietlenie tekstu
+	lcall	lcd_puts
+
+	mov	A, #14h				; x = 4, y = 1
+	lcall	lcd_gotoxy		; przejscie do pozycji (4, 1)
+
+	mov	DPTR, #text_number	; wyswietlenie tekstu
+	lcall	lcd_puts
+
+	mov	A, #12				; wyswietlenie liczby
+	lcall	lcd_dec_2
+
+	sjmp	$
+
+;=====================================================================
+
+;---------------------------------------------------------------------
+; Zapis komendy
+;
+; Wejscie: A - kod komendy
+;---------------------------------------------------------------------
+lcd_write_cmd:
+	push DPH
+	push DPL
+
+	push ACC					; zapisuje komende na stosie
+	mov DPTR, #RD_STAT
+	
+wait_till_busy_cmd:				; czekam, az sterownik bedzie dostepny
+	movx A, @DPTR
+	jb ACC.7, wait_till_busy_cmd
+	
+	pop ACC						; wprowadzam komende
+	mov DPTR, #WR_CMD
+	movx @DPTR, A
+	
+	pop DPL
+	pop DPH
+	ret
+
+;---------------------------------------------------------------------
+; Zapis danych
+;
+; Wejscie: A - dane do zapisu
+;---------------------------------------------------------------------
+lcd_write_data:
+	push DPH
+	push DPL
+
+	push ACC					; zapisuje dane na stosie
+	mov DPTR, #RD_STAT
+	
+wait_till_busy_data:			; czekam, az sterownik bedzie dostepny
+	movx A, @DPTR
+	jb ACC.7, wait_till_busy_data
+	
+	pop ACC						; wprowadzam dane
+	mov DPTR, #WR_DATA
+	movx @DPTR, A
+	
+	pop DPL
+	pop DPH
+	ret
+
+;---------------------------------------------------------------------
+; Inicjowanie wyswietlacza
+;---------------------------------------------------------------------
+lcd_init:
+	
+	mov A, #00111000b			; ustalenie tryby pracy
+	lcall lcd_write_cmd
+	
+	mov A, #00000110b			; ustalenie trybu wprowadzania
+	lcall lcd_write_cmd
+	
+	mov A, #00001111b			; kontrola widocznosci elementu lcd
+	lcall lcd_write_cmd
+	
+	mov A, #00000001b			; wyczyszczenie ekranu
+	lcall lcd_write_cmd
+
+	ret
+
+;---------------------------------------------------------------------
+; Ustawienie biezacej pozycji wyswietlania
+;
+; Wejscie: A - pozycja na wyswietlaczu: ---y | xxxx
+;---------------------------------------------------------------------
+lcd_gotoxy:
+
+	jnb ACC.4, call_command		; sprawdzam, czy potrzebna jest korekta
+	
+	clr ACC.4					; korekta
+	setb ACC.6
+	
+call_command:
+	setb ACC.7					; dopelniam brrakujacy bit komendy
+	lcall lcd_write_cmd
+
+	ret
+
+;---------------------------------------------------------------------
+; Wyswietlenie tekstu od biezacej pozycji
+;
+; Wejscie: DPTR - adres pierwszego znaku tekstu w pamieci kodu
+;---------------------------------------------------------------------
+lcd_puts:
+	
+next_char:					; wyswietlam kolejne bajty w formie kodow ascii
+	clr A
+	movc A, @A+DPTR
+	jz stop_printing
+	lcall lcd_write_data
+	inc DPTR
+	sjmp next_char
+
+stop_printing:
+	ret
+
+;---------------------------------------------------------------------
+; Wyswietlenie liczby dziesietnej
+;
+; Wejscie: A - liczba do wyswietlenia (00 ... 99)
+;---------------------------------------------------------------------
+lcd_dec_2:
+	
+	mov B, #10				; dzielenie przez 10
+	div AB
+	
+	add A, #'0'				; wyswietlenie starszej cyfry
+	lcall lcd_write_data
+	
+	mov A, B				; wyswietlenie mlodszej cyfry
+	add A, #'0'
+	lcall lcd_write_data
+	
+	ret
+
+;---------------------------------------------------------------------
+; Definiowanie wlasnego znaku
+;
+; Wejscie: A    - kod znaku (0 ... 7)
+;          DPTR	- adres tabeli opisu znaku w pamieci kodu
+;---------------------------------------------------------------------
+lcd_def_char:
+
+	ret
+
+text_hello:
+	db	'Hello word', 0
+text_number:
+	db	'Number = ', 0
+
+END
